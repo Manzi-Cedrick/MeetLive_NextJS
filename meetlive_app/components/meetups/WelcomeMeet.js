1346be101af9b827a134246f6dc2d55abef8ee2f
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-07-01 23:02:12 +0200

    welcome page display

--- meetlive_app/components/meetups/WelcomeMeet.js
+++ meetlive_app/components/meetups/WelcomeMeet.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function WelcomeMeet() {
   return (
-    <div className="mx-5 p-5 w-[20vw] h-[20vh] bg-slate-100">
-        <div className="welcome">
+    <div className="mx-5 p-5 mt-5 w-[20vw] h-[20vh] bg-slate-100">
+        <div>
             <span>Welcome to React</span>
             <p>
               How are you feel free to navigate through your meetings and the recent ones!!
