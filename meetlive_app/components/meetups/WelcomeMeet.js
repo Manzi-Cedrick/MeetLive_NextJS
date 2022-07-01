25e33b999da877b08f61e4da54f98262451dde71
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-07-01 21:12:08 +0200

    welcome component

---
+++ meetlive_app/components/meetups/WelcomeMeet.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function WelcomeMeet() {
+  return (
+    <div className="mx-5 p-5 w-[20vw] h-[20vh] bg-slate-100">
+        <div className="welcome">
+            <span>Welcome to React</span>
+            <p>
+              How are you feel free to navigate through your meetings and the recent ones!!
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default WelcomeMeet
