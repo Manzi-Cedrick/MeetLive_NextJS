fa0e655350f343f933eb3afc07501226caad2034
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-07-02 01:41:15 +0200

    detailed display design

---
+++ meetlive_app/components/meetups/MeetDetailed.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const MeetDetailed = () => {
+  return (
+    <div className=" m-auto p-5">
+        <img src="https://media.timeout.com/images/105883842/750/422/image.jpg" alt="No image" className="h-[65vsdh] m-auto w-ful]l" />
+        <div className="m-auto text-center">
+            <p>Meeting Names</p>
+            <p>Description</p>
+            <p>Date</p>
+        </div>
+    </div>
+  )
+}
