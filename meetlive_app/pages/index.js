0f05f6d1609e7ec85e8ebb7834bcd5d121b89858
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-07-02 01:42:00 +0200

    added the fetching

--- meetlive_app/pages/index.js
+++ meetlive_app/pages/index.js
@@ -1,8 +1,53 @@
 
 import UiCards from '../components/meetups/UiCards'
-
-export default function Home() {
+const DummyMeetUps = [
+  {
+    id: 1,
+    name: "Kigali Convention Meetings",
+    schedule: "Manzi Cedrick",
+    date: "09/12/2005",
+    location: "Kigali,Rwanda",
+  },
+  {
+    id: 2,
+    name: "Kigali Management Meeting",
+    schedule: "Cedrick",
+    date: "09/12/2005",
+    location: "Kigali,Rwanda",
+  },
+  {
+    id: 3,
+    name: "Kigali Management Meeting",
+    schedule: "Cedrick",
+    date: "09/12/2005",
+    location: "Kigali,Rwanda",
+  },
+  {
+    id: 4,
+    name: "Kigali Management Meeting",
+    schedule: "Cedrick",
+    date: "09/12/2005",
+    location: "Kigali,Rwanda",
+  },
+  {
+    id: 5,
+    name: "Kigali Management Meeting",
+    schedule: "Cedrick",
+    date: "09/12/2005",
+    location: "Kigali,Rwanda",
+  },
+];
+export default function Home(props) {
   return (
-    <UiCards/>
+    <UiCards meetUp={props.meetUp}/>
   )
+}
+export async function getServerSideProps(context){
+  const req = context.req;
+  const res = context.res;
+  return {
+    props :{
+      meetUp : DummyMeetUps  
+    }
+  }
 }
