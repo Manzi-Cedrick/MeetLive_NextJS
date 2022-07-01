3491cd378425d96ff8bbf84dc0b8e664176e4fe0
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-07-01 23:01:40 +0200

    meetup data

--- meetlive_app/components/meetups/UiCards.js
+++ meetlive_app/components/meetups/UiCards.js
@@ -1,15 +1,52 @@
-import React from 'react'
-import { MeetUp } from './MeetUp'
-import WelcomeMeet from './WelcomeMeet'
+import React from "react";
+import { MeetUp } from "./MeetUp";
+import WelcomeMeet from "./WelcomeMeet";
 
 function UiCards() {
+  const DummyMeetUps = [
+    {
+      id: 1,
+      name: "Kigali Convention Meetings",
+      schedule: "Manzi Cedrick",
+      date: "09/12/2005",
+      location: "Kigali,Rwanda",
+    },
+    {
+      id: 2,
+      name: "Kigali Management Meeting",
+      schedule: "Cedrick",
+      date: "09/12/2005",
+      location: "Kigali,Rwanda",
+    },
+    {
+      id: 3,
+      name: "Kigali Management Meeting",
+      schedule: "Cedrick",
+      date: "09/12/2005",
+      location: "Kigali,Rwanda",
+    },
+    {
+      id: 4,
+      name: "Kigali Management Meeting",
+      schedule: "Cedrick",
+      date: "09/12/2005",
+      location: "Kigali,Rwanda",
+    },
+    {
+      id: 5,
+      name: "Kigali Management Meeting",
+      schedule: "Cedrick",
+      date: "09/12/2005",
+      location: "Kigali,Rwanda",
+    },
+  ];
   return (
-    <div className="flex m-5 justify-center ">
-        <WelcomeMeet/>
-        <MeetUp/>
-        <WelcomeMeet/>
-    </div>  
-  )
+    <div className="flex bg-slate-300 justify-center ">
+      <WelcomeMeet />
+      <MeetUp meetups={DummyMeetUps} />
+      <WelcomeMeet />
+    </div>
+  );
 }
 
-export default UiCards
+export default UiCards;
