919883758fce65eaf6047aade9f26ea3fba3fb15
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-07-02 11:23:40 +0200

    the get static function

--- meetlive_app/pages/[newsDetails]/index.js
+++ meetlive_app/pages/[newsDetails]/index.js
@@ -1,9 +1,38 @@
-//our-domain.com/newDetails
-
+// import { useRouter } from "next/router"
 import { MeetDetailed } from "../../components/meetups/MeetDetailed"
 
-export const DetailedPage = () => {
+export default function DetailedPage (props) {
   return (
-    <MeetDetailed/>
+    <MeetDetailed meetupsData={props.meetupsData} />
   )
+}
+export async function getStaticPaths() {
+    return {
+        fallback : true,
+        paths: [
+            {
+                params:{
+                    newsDetails : '1'
+                }
+            }, {
+                params:{
+                    newsDetails : '2'
+                }
+            }
+        ],
+    }
+}
+export async function getStaticProps(context) {
+    const meetUpId = context.params.newsDetails;
+    return {
+        props:{
+            meetupsData :{
+                id : meetUpId,
+                image : 'https://media.timeout.com/images/105883842/750/422/image.jpg',
+                name : 'Meet Kigali',
+                address : 'Kigali,Rwanda',
+                description : 'Meet Kigali Now Rwanda right now Kigali rwanda Meet Kigali Now Rwanda right now Kigali rwandaMeet Kigali Now Rwanda right now Kigali rwanda'
+            }
+        }
+    }
 }
