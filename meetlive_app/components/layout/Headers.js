6cca523187bdd2999c7168b780ff3ab97521c45a
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-07-02 11:22:28 +0200

    restyling the logo

--- meetlive_app/components/layout/Headers.js
+++ meetlive_app/components/layout/Headers.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Headers(){
   return (
     <header className="flex bg-violet-500 justify-between text-white place-items-center  h-[8vh] p-5">
-      <div className="logo w-12 h-12 rounded-full bg-green-500"> 
+      <div className="logo w-12 h-12 rounded-full "> 
       </div>
       <ul className="links  flex  gap-2 text-[14px]">
         <li>Recent Meetings</li>
