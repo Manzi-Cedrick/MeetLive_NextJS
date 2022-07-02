4829ce2b4837410366f5868566953ab7a74829e8
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-07-03 00:10:04 +0200

    retrieve the data

--- meetlive_app/pages/api/hello.js
+++ meetlive_app/pages/api/hello.js
@@ -1,5 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import connectionEstablish from '../../../utils/connect';
+import MeetUp from '../../../models/MeetUpModel';
 
+/**
+ * @param {import('next').NextApiRequest} req
+ * @param {import('next').NextApiResponse} res
+ */   
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+    await connectionEstablish()
+    try {
+        const retrievedData = await MeetUp.find({});
+        if (!retrievedData) throw new Error('No Data fetched :',error.message)
+        res.status(200).json({success :"ok",retrievedData: retrievedData});
+    }catch (error) {
+        res.status(404);
+        throw new Error(error.message);
+    }
 }
