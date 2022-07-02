a2679121b1286636a6620fdb68e9cfe4ceb9954b
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-07-02 14:16:01 +0200

    connection optimization

--- meetlive_app/utils/connect.js
+++ meetlive_app/utils/connect.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 export default async function connectionEstablish (){
     try{
-        await mongoose.connect(process.env.mongodb,()=>{
+        await mongoose.connect(`${process.env.mongodbs}`,()=>{
             console.log("Database successfuly connected")
         })
     }catch(error){
