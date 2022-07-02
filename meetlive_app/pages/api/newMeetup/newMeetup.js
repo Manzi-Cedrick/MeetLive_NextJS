de891c49369191645772672849131c6fdb6a9f22
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-07-02 13:02:33 +0200

    Add MeetUp

---
+++ meetlive_app/pages/api/newMeetup/newMeetup.js
@@ -0,0 +1,25 @@
+import connectionEstablish from '../../../utils/connect';
+import MeetUp from '../../../models/MeetUpModel';
+
+/**
+ * @param {import('next').NextApiRequest} req
+ * @param {import('next').NextApiResponse} res
+ */
+export default async function AddMeetUp(req, res){
+    // const data = req.body;
+    // console.log(req.body);
+    // const {name,image,description,address} = data;
+    connectionEstablish()
+    try {
+        const addedData = await MeetUp.create({
+            title: req.body.title,
+            address: req.body.address,
+            description: req.body.description,
+            image: req.body.image,
+        })
+        res.status(200).json({success :"ok",data: addedData});
+    }catch (error) {
+        res.status(404);
+        throw new Error(error.message);
+    }
+}
